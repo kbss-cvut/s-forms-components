77ad6617ce6cd57dd5cb36bdd1101e817565c4c1
Vojtěch Luňák <lunakvoj@fel.cvut.cz>
2023-07-26 13:43:53 +0200

    [Upd #31] Storybook upgraded to v7 to fix node 18 errors on netlify

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import "../src/styles/components.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@kbss-cvut/s-forms/dist/s-forms.css";
+import "../node_modules/@kbss-cvut/s-forms/dist/s-forms.css";
 import "intelligent-tree-select/lib/styles.css";
 
 export const parameters = {
@@ -13,3 +13,8 @@ export const parameters = {
     },
   },
 };
+
+const preview = {
+  decorators: [(Story, context) => <Story />],
+};
+export default preview;
